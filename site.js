954d4c80b9236e9b4a98055897cdda60ab6d0994
ardalan@mobilefringe.com @ CodeCloud <ardalan@mobilefringe.com @ CodeCloud>
2017-09-26 17:36:56 -0400

    The Landing at Renton 1.4  /site.js

--- site.js
+++ site.js
@@ -2,5 +2,5 @@ var site_json = {
                     "name" : "The Landing at Renton",
                     "default_image" : "//codecloud.cdn.speedyrails.net/sites/59c533e36e6f6448251c0000/image/jpeg/1505748380000/land_500x500_logo.jpg",
                     "time_zone" : "T08:00:00Z",
-                    "social_feed" : "//thelanding.mallmaverick.com/api/v2/landing/social.json"
+                    "social_feed" : "//thelanding.mallmaverick.com/api/v4/landing/social.json"
                 }
